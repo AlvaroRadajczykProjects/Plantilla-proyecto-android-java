879604aead114aa7ad923cd9f30088cc4cf63da2
Álvaro Radajczyk <40389952+AlvaroRadajczykProjects@users.noreply.github.com>
2023-10-14 01:16:31 +0200

    broadcastreceiver_1

--- app/src/main/java/com/example/plantillaproyectoandroidjava/MainActivity.java
+++ app/src/main/java/com/example/plantillaproyectoandroidjava/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.plantillaproyectoandroidjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BroadcastReceiver br = new MyBroadcastReceiver();
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        this.registerReceiver(br, filter);
+
     }
 }
